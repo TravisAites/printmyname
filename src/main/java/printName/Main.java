74a10dfeae4895dfefa95c697eeb32833c6f5a43
travisaites21@gmail.com <>
2020-02-04 16:20:17 -0500

    printname first commit

---
+++ src/main/java/printName/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package printName;
+
+/**
+ *
+ * @author travi
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Aites();
+    }
+    
+    public static void Aites(){
+    System.out.println("Travis Aites");
+    }
+}
